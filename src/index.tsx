1ca7dfd60007842a2160cf69d7a30ac9ad3b239a
ME-SKY <meskalitofly@gmail.com>
2021-03-26 02:18:51 +1000

    Init project with webpack/scss/typescript using https://createapp.dev/webpack, set config, add hot-reloading

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {createContext, useContext} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+// import {Store} from "./store/store";
 
+// const store = new Store([]);
+// @ts-ignore
+// const StoreContext = createContext<Store>([]);
+// export const useStore = (): Store => useContext(StoreContext);
+
+// @ts-ignore
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    //@ts-ignore
+        <App/>,
+    document.getElementById('app')
 );
+
+{/*<StoreContext.Provider value={store}>*/}
+{/*    <App/>*/}
+// </StoreContext.Provider>,
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.tsx'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    // new CopyPlugin({
+    //   patterns: [{ from: 'src/index.html' }],
+    // }),
+    new HtmlWebpackPlugin({
+      appMountId: 'app',
+      filename: 'index.html',
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ],
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
+};
+
+module.exports = (env, argv) => {
+  if (argv.hot) {
+    // Cannot use 'contenthash' when hot reloading is enabled.
+    config.output.filename = '[name].[hash].js';
+  }
+
+  return config;
+};
